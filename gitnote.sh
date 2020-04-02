1314f617e525c3fd73457fac6f307f1cedde404f
yuqing.ji <yuqing.ji@outlook.com>
2020-04-03 07:58:26 +0800

    update

--- gitnote.sh
+++ gitnote.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ $0 == 'c' ]]; then
+if [[ $1 == 'c' ]]; then
   git pull; git add .; git commit -m 'update';
-elif [[ $0 == 'p' ]]; then
+elif [[ $1 == 'p' ]]; then
   git push origin master;
 else
   git pull; git add .; git commit -m 'update'; git push origin master;

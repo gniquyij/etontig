36bff8eea24e95ee29e64f65d130299d35159ad1
yuqing.ji <yuqing.ji@outlook.com>
2020-04-03 07:57:52 +0800

    update

--- gitnote.sh
+++ gitnote.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
-while getopts 'cp' opt
-do
-  case ${opt} in
-  c) commit=${OPTARG};;
-  p) push=${OPTARG};;
-  *) echo ${opt}' is not an option';;
-  esac
-done
-
-if [[ ${option} == 'commit' ]]; then
+if [[ $0 == 'c' ]]; then
   git pull; git add .; git commit -m 'update';
-elif [[ ${option} == 'push' ]]; then
+elif [[ $0 == 'p' ]]; then
   git push origin master;
 else
   git pull; git add .; git commit -m 'update'; git push origin master;

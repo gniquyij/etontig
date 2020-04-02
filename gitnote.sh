07b40db669c513edff890e77790c0ab44d7bc45a
yuqing.ji <yuqing.ji@outlook.com>
2020-04-03 07:31:24 +0800

    update

--- gitnote.sh
+++ gitnote.sh
@@ -11,9 +11,9 @@ done
 
 export cmd=`git pull; git add .; git commit -m 'update'; git push origin master;`
 if [[ ${option} == 'commit' ]]; then
-  cmd = `git pull; git add .; git commit -m 'update';`
+  export cmd = `git pull; git add .; git commit -m 'update';`
 elif [[ ${option} == 'push' ]]; then
-  cmd = `git push origin master;`
+  export cmd = `git push origin master;`
 fi
 
 echo $cmd

36d836c85cc47a35c4160346e764c8a5bdac1e14
yuqing.ji <yuqing.ji@outlook.com>
2020-04-04 16:28:15 +0800

    update

--- etontig.sh
+++ etontig.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+if [ "$2" ]; then
+  branch=$2
+fi
+
+branch_check=$(git show-ref --quiet refs/heads/$branch; echo $?)
+if [ $branch_check == 1 ]; then
+  echo 'Nonexistent branch!'
+  exit 1
+fi
 
 if [[ $1 == 'c' ]]; then
-  git pull; git add .; git commit -m 'update';
+  git checkout $branch; git pull; git add . ; git commit -m 'update';
 elif [[ $1 == 'p' ]]; then
-  git push origin $current_branch;
+  git push origin $branch;
 else
-  git pull; git add .; git commit -m 'update'; git push origin $current_branch;
+  git pull; git add .; git commit -m 'update'; git push origin $branch;
 fi

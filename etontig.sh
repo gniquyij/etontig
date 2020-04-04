10d5d55794d53041617c8b30a950a27624f63b5e
yuqing.ji <yuqing.ji@outlook.com>
2020-04-04 13:14:26 +0800

    update

--- etontig.sh
+++ etontig.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+current_branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
 if [[ $1 == 'c' ]]; then
   git pull; git add .; git commit -m 'update';
 elif [[ $1 == 'p' ]]; then
-  git push origin master;
+  git push origin $current_branch;
 else
-  git pull; git add .; git commit -m 'update'; git push origin master;
+  git pull; git add .; git commit -m 'update'; git push origin $current_branch;
 fi
